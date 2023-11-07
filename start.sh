65d103c90ab791807ecfc8d48b1470b2610f79c5
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-07 14:03:11 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+dpkg -l
+
+echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
+
+. /etc/apache2/envvars
+exec /usr/sbin/apache2 -DFOREGROUND
