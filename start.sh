165c86b51987dc99fed5e9bc14300bc781e389c0
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-10 13:01:08 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,19 +7,13 @@ set -x
 ldd `which memcached` | grep -i sasl
 
 find / -name *memcached* -print
-cat /etc/memcached.conf
-# sasldb_path: /tmp/memcached.sasldb
-echo "sasldb_path: /tmp/memcached.sasldb" >>/etc/memcached.conf
-cat /etc/memcached.conf
 
 /usr/bin/memcached --help
 useradd memcached -G sasl
 saslpasswd2 --help
 echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -a memcached -c memcached -f /tmp/memcached.sasldb
 chown memcached:memcached /tmp/memcached.sasldb
-# /usr/bin/memcached -vvv -B binary -d -u memcached -S
-# /usr/bin/memcached -S -vvvv -B binary -d -u memcached
-systemctl start memcached
+MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb /usr/bin/memcached -S -vvvv -B binary -d -u memcached
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
