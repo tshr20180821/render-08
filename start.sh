b69e8e3b15001263264ef2b1dcb5a4ed8c1b8248
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-10 09:43:38 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ set -x
 
 dpkg -l
 
-nproc
+/usr/bin/memcached --help
+/usr/bin/memcached -vvv -B binary -d -u memcache
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
