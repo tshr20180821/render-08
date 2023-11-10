f98e96eb02fd499f5c3c5a6cc3879070e03faa4a
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-11 07:52:58 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,17 +8,17 @@ set -x
 /usr/bin/memcached --help
 useradd memcached -G sasl
 saslpasswd2 --help
-echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -f /tmp/memcached.sasldb -a memcached -c memcached
+# echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -f /tmp/memcached.sasldb -a memcached -c memcached
 echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -a memcached -c memcached
-chown memcached:memcached /tmp/memcached.sasldb
+# chown memcached:memcached /tmp/memcached.sasldb
 # cat /tmp/memcached.sasldb
 sasldblistusers2
-export MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb
+# export MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb
 
 export SASL_CONF_PATH=/tmp/memcached.conf
 echo "mech_list: login plain anonymous ntlm scram cram-md5 digest-md5" >/tmp/memcached.conf
 # echo "mech_list: login plain anonymous ntlm scram cram-md5 digest-md5" >/tmp/memcached.conf
-echo "sasldb_path: /tmp/memcached.sasldb" >>/tmp/memcached.conf
+# echo "sasldb_path: /tmp/memcached.sasldb" >>/tmp/memcached.conf
 cat /tmp/memcached.conf
 /usr/sbin/saslauthd --help
 /usr/sbin/saslauthd -a sasldb -V
