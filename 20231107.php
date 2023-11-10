60f099378ed139e38b4dde899e4908ad7e7c281a
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-10 10:16:20 +0900

    Update 20231107.php

--- 20231107.php
+++ 20231107.php
@@ -6,6 +6,11 @@ $m = new Memcached();
 $m->addServer('127.0.0.1', 11211);
 
 $m->set('KEY_A', 'DATA_A');
+error_log($m->getResultCode());
+error_log($m->getResultMessage());
+error_log('DATA : ' . $m->get('KEY_A'));
+error_log($m->getResultCode());
+error_log($m->getResultMessage());
 
 echo $m->get('KEY_A');
 
