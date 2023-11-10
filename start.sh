123145e17b1c141bb916a47b445655664dce56d0
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-10 15:53:45 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,10 @@ useradd memcached -G sasl
 saslpasswd2 --help
 echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -a memcached -c memcached -f /tmp/memcached.sasldb
 chown memcached:memcached /tmp/memcached.sasldb
+cat /tmp/memcached.sasldb
 export MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb
 export SASL_CONF_PATH=/etc/memcached.conf
+cat /etc/memcached.conf
 /usr/bin/memcached -S -vvvv -B binary -d -u memcached
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
