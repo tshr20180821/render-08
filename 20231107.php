9a788435a73dc42313493aab3554a7992c33f1cb
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-10 10:37:27 +0900

    Update 20231107.php

--- 20231107.php
+++ 20231107.php
@@ -3,14 +3,13 @@
 error_log('START');
 
 $m = new Memcached();
+$m->setOption(OPT_BINARY_PROTOCOL, true);
 $m->addServer('127.0.0.1', 11211);
 
 $m->set('KEY_A', 'DATA_A');
 error_log($m->getResultCode());
 error_log($m->getResultMessage());
 error_log('DATA : ' . $m->get('KEY_A'));
-error_log($m->getResultCode());
-error_log($m->getResultMessage());
 
 echo $m->get('KEY_A');
 
