5798564e569474eaacb32d0cc119f3cf162a3686
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-10 16:00:35 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,9 +9,13 @@ useradd memcached -G sasl
 saslpasswd2 --help
 echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -a memcached -c memcached -f /tmp/memcached.sasldb
 chown memcached:memcached /tmp/memcached.sasldb
-cat /tmp/memcached.sasldb
+# cat /tmp/memcached.sasldb
 export MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb
-export SASL_CONF_PATH=/etc/memcached.conf
+
+echo "mech_list: plain cram-md5" >/tmp/memcached.conf
+echo "sasldb_path: /tmp/test-memcached.sasldb" >>/tmp/memcached.conf
+
+export SASL_CONF_PATH=/tmp/memcached.conf
 cat /etc/memcached.conf
 /usr/bin/memcached -S -vvvv -B binary -d -u memcached
 
