f41d005b3699281103654f3577061fa18918bd9b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-12 07:51:30 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,21 +7,21 @@ set -x
 /usr/bin/memcached --help
 useradd memcached -G sasl
 saslpasswd2 --help
-# echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -f /tmp/memcached.sasldb -a memcached -c memcached
-echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -a memcached -c memcached
-# chown memcached:memcached /tmp/memcached.sasldb
-chown memcached:memcached /etc/sasldb2
-# cat /tmp/memcached.sasldb
+export SASL_DB_PATH=/tmp/memcached.sasldb
+echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -f ${SASL_DB_PATH} -a memcached -c memcached
+# echo ${RENDER_EXTERNAL_HOSTNAME} | saslpasswd2 -p -a memcached -c memcached
+chown memcached:memcached /tmp/memcached.sasldb
+# chown memcached:memcached /etc/sasldb2
 sasldblistusers2
-# export MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb
+export MEMCACHED_SASL_PWDB=/tmp/memcached.sasldb
 
 export SASL_CONF_PATH=/tmp/memcached.conf
 echo "mech_list: plain cram-md5" >/tmp/memcached.conf
 # echo "mech_list: login plain anonymous ntlm scram cram-md5 digest-md5" >/tmp/memcached.conf
-# echo "sasldb_path: /tmp/memcached.sasldb" >>/tmp/memcached.conf
+echo "sasldb_path: /tmp/memcached.sasldb" >>/tmp/memcached.conf
 cat /tmp/memcached.conf
 /usr/sbin/saslauthd --help
-export SASL_DB_PATH=/tmp/dummy.sasldb
+# export SASL_DB_PATH=/tmp/dummy.sasldb
 /usr/sbin/saslauthd -a sasldb -V
 
 testsaslauthd -u memcached -p ${RENDER_EXTERNAL_HOSTNAME}
