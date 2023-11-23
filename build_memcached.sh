310dd1b812589c6f18ce9dec7a8bf8ee2b71b30c
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-23 12:41:15 +0900

    Update build_memcached.sh

--- build_memcached.sh
+++ build_memcached.sh
@@ -58,7 +58,8 @@ ccache -s
 
 # find / -name openssl.pc -print
 
-export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig;${PKG_CONFIG_PATH}
+echo ${PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig/;${PKG_CONFIG_PATH}
 
 curl -LO https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz
 
