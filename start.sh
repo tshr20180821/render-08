59b18a0847a69543bf59db8418e1cd388c9cc981
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-23 10:58:30 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ set -x
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
-# for i in {1..60} ; do sleep 30s && ps aux ; done &
-# cp ./build_memcached.sh /tmp/
-# time /tmp/build_memcached.sh 1>&2 &
+for i in {1..60} ; do sleep 120s && curl https://${RENDER_EXTERNAL_HOSTNAME}/ ; done &
+cp ./build_memcached.sh /tmp/
+time /tmp/build_memcached.sh &
 
 . /etc/apache2/envvars >/dev/null 2>&1
 exec /usr/sbin/apache2 -DFOREGROUND
