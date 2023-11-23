405620d2b6bba69692f2e3b61599521b31917914
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-23 10:28:21 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@ set -x
 
 dpkg -l
 
-ls -lang /tmp
-
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
+cp ./build_memcached.sh /tmp/
 time /tmp/build_memcached.sh &
 
 . /etc/apache2/envvars 2>&1 >/dev/null
