fc146465951529222ff59cff7f31e98ca760d0eb
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-23 10:36:14 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ dpkg -l
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
+for i in {1..60} ; do sleep 30s && ps aux ; done &
 cp ./build_memcached.sh /tmp/
 time /tmp/build_memcached.sh 1>&2 &
 
