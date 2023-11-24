d77b7715016d87a1bfe56125cae350929fb14344
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-24 09:36:16 +0900

    Update build_apache2.sh

--- build_apache2.sh
+++ build_apache2.sh
@@ -5,3 +5,26 @@ set -e
 export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
+
+APACHE_VERSION=2.4.58
+
+pushd /tmp
+
+curl -O https://ftp.jaist.ac.jp/pub/apache//httpd/httpd-${APACHE_VERSION}.tar.bz2
+
+tar xf httpd-${APACHE_VERSION}.tar.bz2
+target=httpd-${APACHE_VERSION}
+pushd ${target}
+./configure --help
+# ./configure --prefix=/tmp/usr \
+#   --enable-mods-shared="few" \
+#   --enable-brotli --enable-file-cache \
+#   --disable-authn-core --disable-authn-file --disable-access-compat --disable-authn-core \
+#   --disable-authz-core --disable-authz-host --disable-authz-user --disable-authz-groupfile --disable-auth-basic \
+#   --disable-autoindex --disable-alias --disable-dir --disable-env --disable-filter --disable-headers \
+#   --disable-log_config --disable-mime --disable-reqtimeout --disable-setenvif --disable-status --disable-unixd --disable-version
+./configure
+time make
+make install
+
+popd
