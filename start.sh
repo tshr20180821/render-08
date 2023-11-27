85a207295494de4bf5497a10b69c8eea42cc98c2
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-28 08:35:11 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,16 @@ set -x
 
 # dpkg -l
 
+curl -O http://ftp.us.debian.org/debian/pool/main/a/apache2/apache2_2.4.58-1_amd64.deb
+dpkg -i apache2_2.4.58-1_amd64.deb
+
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
-for i in {1..60} ; do sleep 120s && ps aux && curl https://${RENDER_EXTERNAL_HOSTNAME}/ ; done &
+# for i in {1..60} ; do sleep 120s && ps aux && curl https://${RENDER_EXTERNAL_HOSTNAME}/ ; done &
 # cp ./build_memcached.sh /tmp/
 # time /tmp/build_memcached.sh &
-cp ./build_apache2.sh /tmp/
-time /tmp/build_apache2.sh 2>&1 | tee -a /var/www/html/build_log.txt &
+# cp ./build_apache2.sh /tmp/
+# time /tmp/build_apache2.sh 2>&1 | tee -a /var/www/html/build_log.txt &
 
 . /etc/apache2/envvars >/dev/null 2>&1
 exec /usr/sbin/apache2 -DFOREGROUND
