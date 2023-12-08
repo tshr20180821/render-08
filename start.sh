d90921885bc1f5b77cc9972e39a0601dbafcdd72
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-08 16:51:25 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ docker-php-ext-configure
 dragonfly --help
 dragonfly --helpfull
 export DFLY_PASSWORD=testpass999
-dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --colorlogtostderr &
+dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --v --colorlogtostderr &
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
