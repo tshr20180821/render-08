c990c341cbdd1aab5821fe62a90e3a9d150749b3
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-08 17:09:35 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,8 +10,7 @@ set -x
 # dragonfly --help
 # dragonfly --helpfull
 export DFLY_PASSWORD=testpass999
-# dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --colorlogtostderr &
-dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --v &
+dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --colorlogtostderr &
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
