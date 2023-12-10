74ea41393fae43f72fb96e5620faca5a0cf10d84
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-11 08:38:06 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,8 @@ set -x
 
 # dragonfly --help
 # dragonfly --helpfull
-export DFLY_PASSWORD=testpass999
-dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --colorlogtostderr &
+# export DFLY_PASSWORD=testpass999
+# dragonfly --bind=127.0.0.1 --requirepass=${DFLY_PASSWORD} --version_check=false --memcached_port=11212 --tcp_keepalive=120 --port 6380 --colorlogtostderr &
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
@@ -20,13 +20,13 @@ echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_n
 # cp ./build_apache2.sh /tmp/
 # time /tmp/build_apache2.sh 2>&1 | tee -a /var/www/html/build_log.txt &
 
-curl "${UPSTASH_REDIS_REST_URL}/set/foo/bar" \
+curl -sS "${UPSTASH_REDIS_REST_URL}/set/foo/bar" \
   -H "Authorization: Bearer ${UPSTASH_REDIS_REST_TOKEN}"
 
-curl "${UPSTASH_REDIS_REST_URL}/get/foo" \
+curl -sS "${UPSTASH_REDIS_REST_URL}/get/foo" \
   -H "Authorization: Bearer ${UPSTASH_REDIS_REST_TOKEN}"
   
-curl "${UPSTASH_REDIS_REST_URL}/get/boo" \
+curl -v "${UPSTASH_REDIS_REST_URL}/get/boo" \
   -H "Authorization: Bearer ${UPSTASH_REDIS_REST_TOKEN}"
   
 sleep 5s && ss -anpt && ps aux &
