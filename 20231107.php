63748ef34b1c7342c511e464cca4a037bfbd6c52
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-13 13:35:13 +0900

    Update 20231107.php

--- 20231107.php
+++ 20231107.php
@@ -8,7 +8,7 @@ $hostname = parse_url(getenv('UPSTASH_REDIS_REST_URL'),  PHP_URL_HOST);
 error_log($hostname);
 $port = explode('.', explode('-', $hostname)[3])[0];
 error_log($port);
-$redis->connect('tlsv1.2://' . $hostname, $port);
+$redis->connect('tlsv1.3://' . $hostname, $port);
 $redis->auth(getenv('UPSTASH_REDIS_PASSWORD'));
 
 error_log($redis->get('APT_RESULT_' . getenv('RENDER_EXTERNAL_HOSTNAME')));
