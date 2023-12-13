76d1cc05ec60c1a6958d50c71a6f25d7c5815540
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-13 11:14:55 +0900

    Update 20231107.php

--- 20231107.php
+++ 20231107.php
@@ -2,16 +2,15 @@
 
 error_log('START');
 
-//$redis = new Redis();
+$redis = new Redis();
 
 $hostname = parse_url(getenv('UPSTASH_REDIS_REST_URL'),  PHP_URL_HOST);
 error_log($hostname);
-preg_match('/(\d+)/', $hostname, $matches, PREG_OFFSET_CAPTURE);
-error_log(print_r($matches, true));
-//$redis->connect('tlsv1.2://' . $hostname, $port);
-//$redis->auth(getenv('UPSTASH_REDIS_REST_TOKEN'));
+$port = explode('-', $hostname)[3];
+error_log($port);
+$redis->connect('tlsv1.2://' . $hostname, $port);
+$redis->auth(getenv('UPSTASH_REDIS_REST_TOKEN'));
 
-//print_r($redis->get("foo"));
-
+error_log(print_r($redis->get('APT_RESULT_' . getenv('RENDER_EXTERNAL_HOSTNAME')), true));
 
 error_log('FINISH');
