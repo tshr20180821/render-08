b0e2e674c16edb5397395ae9b24be9d1e09a79c8
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-18 02:38:21 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -43,12 +43,11 @@ cat /etc/apache2/mods-available/deflate.conf
 
 find /etc/apache2/mods-available/ -name *.conf -exec cat '{}' ';'
 
-curl https://packages.debian.org/bookworm/apache2 | grep '<h1>'
-curl https://packages.debian.org/bookworm-updates/apache2 | grep '<h1>'
-curl https://packages.debian.org/bookworm-backports/apache2 | grep '<h1>'
-curl https://packages.debian.org/trixie/apache2 | grep '<h1>'
-curl https://packages.debian.org/sid/apache2 | grep '<h1>'
-
+curl -sS https://packages.debian.org/bookworm/apache2 | grep '<h1>' | grep 'apache2'
+curl -sS https://packages.debian.org/bookworm-updates/apache2 | grep '<h1>' | grep 'apache2'
+curl -sS https://packages.debian.org/bookworm-backports/apache2 | grep '<h1>' | grep 'apache2'
+curl -sS https://packages.debian.org/trixie/apache2 | grep '<h1>' | grep 'apache2'
+curl -sS https://packages.debian.org/sid/apache2 | grep '<h1>' | grep 'apache2'
 
 . /etc/apache2/envvars >/dev/null 2>&1
 exec /usr/sbin/apache2 -DFOREGROUND
