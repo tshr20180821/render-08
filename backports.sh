52e0ddc0c360daecd36f2c85121e6aa59545171f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-18 08:57:31 +0900

    Update backports.sh

--- backports.sh
+++ backports.sh
@@ -2,6 +2,6 @@
 
 set +x
 
-echo "${1}" | tee -a "${2}"
+echo "check ${1}" | tee -a "${2}"
 curl -sS -m 10 "https://packages.debian.org/bookworm-backports/${1}" 2>>"${2}" | grep '<h1>' | grep "${1}" | cut -c 14- | tee -a "${2}"
 sleep 2s
